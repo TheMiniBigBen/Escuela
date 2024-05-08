ab309df14af7da203f3f7fd09b78259ac5d9381e
TheMiniBigBen <Angel_3141230041@utd.edu.mx>
2024-05-08 11:23:04 -0600

    Create calculo de pi.py

---
+++ calculo de pi.py
@@ -0,0 +1,18 @@
+import random
+import math
+
+cuantos = 1000
+cuantosSi = 0
+
+for i in range(cuantos):
+    x=random.random()
+    y=random.random()
+    
+    YCalculada = math.sqrt(1-x*x)
+    if y>YCalculada:
+        None
+    else:
+        cuantosSi += 1  
+cuartoDeArea = float(cuantosSi) / float(cuantos)
+
+print(cuartoDeArea)
